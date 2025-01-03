5a3767830d1532a0c9aa44ffb89e3a4da03b26e0
Kinz <161678979+kaniz-naqvi@users.noreply.github.com>
2025-01-03 21:19:42 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-gsap.from("#img1",{
-    delay:0.5,
-    opacity:0, 
-    duration:1,
-    y:60
+gsap.from(".image1", {
+  delay: 0.5,
+  opacity: 0,
+  duration: 1,
+  y: 60,
 });
-gsap.from("#img2",{
-    delay:0.5,
-    opacity:0, 
-    duration:1,
-    x:60
+gsap.from(".image2", {
+  delay: 0.5,
+  opacity: 0,
+  duration: 1,
+  x: 60,
 });
-gsap.from("#img3",{
-    delay:0.5,
-    opacity:0, 
-    duration:1,
-    y:-60
-})
-gsap.from("main h1",{
-    delay:0.5,
-    opacity:0, 
-    duration:1
+gsap.from(".image3", {
+  delay: 0.5,
+  opacity: 0,
+  duration: 1,
+  y: -60,
 });
-gsap.from(".main-p",{
-    delay:0.5,
-    opacity:0, 
-    duration:1
+gsap.from("main .front-div", {
+  delay: 0.5,
+  opacity: 0,
+  duration: 1,
+});
+
+gsap.from(".black .heading,.black .after-dot, .black p, .black .btn", {
+  opacity: 0,
+  stagger: 0.4,
+  scrollTrigger: {
+    trigger: ".black .heading",
+    scroll: "body",
+    start: "top 60%",
+  },
 });
